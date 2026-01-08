fe4b3348c9d08ee0481a533ddf545de5ab9c297d
YeremiSantiago <hernandezysr@gmail.com>
2026-01-08 18:27:59 -0400

    Create domain entities and enums

---
+++ WalletWise.Domain/Entities/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WalletWise.Domain.Common;
+
+namespace WalletWise.Domain.Entities
+{
+    public class Category : BaseEntity<int>
+    {
+        public string Name { get; set; }
+        public int IdUser { get; set; }
+    }
+}
---
+++ WalletWise.Domain/Entities/Transaction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WalletWise.Domain.Common;
+using WalletWise.Domain.Common.Enums;
+
+namespace WalletWise.Domain.Entities
+{
+     public class Transaction : BaseEntity<int>
+    {
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+        public TypeTransaction TypeTransaction { get; set; }
+        public string? Comment { get; set; }
+        public Category CategoryId { get; set; }
+        public int UserId { get; set; }
+
+    }
+}
